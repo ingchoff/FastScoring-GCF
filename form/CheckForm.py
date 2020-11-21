40eaf59cea7bfe718a6c4ed995b22f7193ee9c7a
ingchoff <ingkaratt@gmail.com>
2020-11-21 10:46:16 +0700

    :sparkles:add checkform function

---
+++ form/CheckForm.py
@@ -0,0 +1,53 @@
+import imutils
+import numpy as np
+from imutils import contours
+import cv2
+
+
+def find_contour_circle(form_img, num_choices):
+    thresh = cv2.adaptiveThreshold(form_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                   cv2.THRESH_BINARY_INV, 11, 2 | cv2.THRESH_OTSU)
+    contour = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    contour = imutils.grab_contours(contour)
+    contour = sorted(contour, key=cv2.contourArea, reverse=True)
+    list_circle = []
+    for c in contour:
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.03 * peri, True)
+        (x, y, w, h) = cv2.boundingRect(c)
+        if 15 <= w <= 200 and 15 <= h <= 200 \
+                and len(approx) != 3 and len(approx) != 4 and len(approx) != 5:
+            list_circle.append(c)
+    print(len(list_circle))
+    if len(list_circle) > num_choices:
+        skip = len(list_circle) - num_choices
+        list_circle = list_circle[:-int(skip)]
+    print(len(list_circle))
+    return list_circle
+
+
+def main_process(form_img, column, amount, form_type):
+    available = False
+    form = cv2.imread(form_img)
+    if form_type == "answersheet.png":
+        row = amount / column
+        form_gray = cv2.cvtColor(form, cv2.COLOR_BGR2GRAY)
+        # still fixed choice each question = 5
+        circle_cnts = find_contour_circle(form_gray, 5*column*row)
+        choices_cnts = contours.sort_contours(circle_cnts, method="top-to-bottom")[0]
+        cv2.drawContours(form, choices_cnts, -1, (0, 0, 255), 2)
+        # check this form is compatible with system.
+        if len(circle_cnts) == amount*5:
+            available = True
+    else:
+        row = 10
+        form_gray = cv2.cvtColor(form, cv2.COLOR_BGR2GRAY)
+        circle_cnts = find_contour_circle(form_gray, row*column)
+        choices_cnts = contours.sort_contours(circle_cnts, method="top-to-bottom")[0]
+        cv2.drawContours(form, choices_cnts, -1, (0, 0, 255), 2)
+        if len(circle_cnts) == row*column:
+            available = True
+    return {
+        'bound_img': form,
+        'available': available
+    }
--- main.py
+++ main.py
@@ -4,13 +4,16 @@ from google.cloud import storage, firestore_v1
 import firebase_admin
 from firebase_admin import credentials
 from PIL import Image
+import cv2
 from exam import ImgProcess, Sift
+from form import CheckForm
+
+gcs = storage.Client()
+bucket = gcs.get_bucket(os.environ['CLOUD_STORAGE_BUCKET'])
+db = firestore_v1.Client()
 
 
 def image_process(event, context):
-    gcs = storage.Client()
-    bucket = gcs.get_bucket(os.environ['CLOUD_STORAGE_BUCKET'])
-    db = firestore_v1.Client()
 
     print('Event type: {}'.format(context.event_type))
     print('File: {}'.format(event['name']))
@@ -19,7 +22,7 @@ def image_process(event, context):
 
     list_folder = event['name'].split("/")
     print('{}'.format(list_folder[0]))
-    if list_folder[0] == "exam":
+    if list_folder[0] == "exams":
         list_filename_exam = list_folder[3].split('_')
         # set firestore
         exam_ref = db.collection('exams').document(list_filename_exam[0])
@@ -69,3 +72,70 @@ def image_process(event, context):
         os.remove(formstd_tmp_path)
         os.remove(os.path.join(tempfile.gettempdir(), 'result.jpg'))
         print('{}'.format('final'))
+    if list_folder[0] == "forms":
+        list_filename = list_folder[2].split('_')
+        print('{}'.format(list_filename))
+        form_id = list_filename[0]
+        type_form = list_filename[1]
+        file_name = list_filename[1]
+        if form_id != "analysed":
+            print("{}".format('start checking'))
+            check_form(list_folder, form_id, type_form, file_name)
+
+
+def check_form(list_folder, fid, form_type, filename):
+    # set up firestore
+    form_ref = db.collection('forms').document(fid)
+    snapshot_form = form_ref.get()
+    data_form = snapshot_form.to_dict()
+    # set tmp path for download images
+    form_tmp_path = os.path.join(tempfile.gettempdir(), list_folder[2])
+    if form_type == "answersheet.png":
+        # set blob destination file to download file
+        form_blob = bucket.blob(list_folder[0] + '/' + list_folder[1] + '/' + list_folder[2])
+        form_blob.download_to_filename(form_tmp_path)
+        form_ref.set({
+            'answer_status': 'analysing'
+        }, merge=True)
+        is_available = CheckForm.main_process(form_tmp_path, data_form['column'], data_form['amount'], form_type)
+        if is_available['available']:
+            bound_img_rgb = cv2.cvtColor(is_available['bound_img'], cv2.COLOR_BGR2RGB)
+            analysed_img = Image.fromarray(bound_img_rgb)
+            analysed_img.save(os.path.join(tempfile.gettempdir(), 'analysed.jpg'))
+            analysed_blob = bucket.blob('forms/' + list_folder[1] + '/analysed_' + fid + '_' + filename)
+            analysed_blob.upload_from_filename(os.path.join(tempfile.gettempdir(), 'analysed.jpg'),
+                                               content_type='image/jpeg')
+            form_ref.set({
+                'answer_status': 'pass',
+                'analysed_answersheet_path': 'forms/' + list_folder[1] + '/analysed_' + fid + '_' + filename
+            }, merge=True)
+        else:
+            form_ref.set({
+                'error_ans_msg': 'Not Compatible',
+                'answer_status': 'error'
+            }, merge=True)
+        os.remove(form_tmp_path)
+    if form_type == "student.png":
+        # set blob destination file to download file
+        form_blob = bucket.blob(list_folder[0] + '/' + list_folder[1] + '/' + list_folder[2])
+        form_blob.download_to_filename(form_tmp_path)
+        form_ref.set({
+            'stu_status': 'analysing'
+        }, merge=True)
+        is_available = CheckForm.main_process(form_tmp_path, data_form['stu_column'], data_form['amount'], form_type)
+        if is_available['available']:
+            bound_img_rgb = cv2.cvtColor(is_available['bound_img'], cv2.COLOR_BGR2RGB)
+            analysed_img = Image.fromarray(bound_img_rgb)
+            analysed_img.save(os.path.join(tempfile.gettempdir(), 'analysed.jpg'))
+            analysed_blob = bucket.blob('forms/' + list_folder[1] + '/analysed_' + fid + '_' + filename)
+            analysed_blob.upload_from_filename(os.path.join(tempfile.gettempdir(), 'analysed.jpg'),
+                                               content_type='image/jpeg')
+            form_ref.set({
+                'stu_status': 'pass',
+                'analysed_stu_path': 'forms/' + list_folder[1] + '/analysed_' + fid + '_' + filename
+            }, merge=True)
+        else:
+            form_ref.set({
+                'error_stu_msg': 'Not Compatible',
+                'stu_status': 'error'
+            }, merge=True)
